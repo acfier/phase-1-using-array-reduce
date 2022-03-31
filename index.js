27f297c7bd8591e94d0bb6b1f53d790324d1c947
acfier <acfier@gmail.com>
2022-03-31 11:53:17 -0400

    commit tree 705e93d38ddde1886881366d3e80215f41a67044

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const reducer = (accum, element) => {
+    let item = 0
+    const battery = () => {
+        let totalBatteries = 0
+        totalBatteries = element
+        console.log(totalBatteries)
+        return item += totalBatteries
+    }
+    return accum += item;
+}
+
+let totalBatteries = batteryBatches.reduce(reducer, 31)
